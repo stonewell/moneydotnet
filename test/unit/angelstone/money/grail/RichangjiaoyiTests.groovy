2f95178c3a75b1a45e31d3fd3da1a4015bff8281
jingnan.si <jingnan.si@fb698924-7d51-0410-b70a-75a81c54cbca>
2010-07-06 09:04:09 +0000

    ignore targe folder

---
+++ test/unit/angelstone/money/grail/RichangjiaoyiTests.groovy
@@ -0,0 +1,17 @@
+package angelstone.money.grail
+
+import grails.test.*
+
+class RichangjiaoyiTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
