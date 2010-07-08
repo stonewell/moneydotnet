f12889842fb7ca5fb6e54677c3091223adafc7ec
jingnan.si <jingnan.si@fb698924-7d51-0410-b70a-75a81c54cbca>
2010-07-08 07:49:42 +0000

    add dynamic update fenlei summary by AJAX

--- branches/angelstone.money.grail/grails-app/controllers/angelstone/money/grail/SummaryController.groovy
+++ branches/angelstone.money.grail/grails-app/controllers/angelstone/money/grail/SummaryController.groovy
@@ -1,5 +1,7 @@
 package angelstone.money.grail
 
+import grails.converters.*
+
 class SummaryController {
 
     def summaryService
@@ -9,13 +11,54 @@ class SummaryController {
         def year = now.year
         def month = now.month
         def day = now.date
+        def fangshi = -1
+        def fenlei = -1
 
         if (params.year != null) year = Integer.valueOf(params.year)
         if (params.month != null) month = Integer.valueOf(params.month)
         if (params.day != null) day = Integer.valueOf(params.day)
+        if (params.fangshi != null) fangshi = Integer.valueOf(params.fangshi)
+        if (params.fenlei != null) fenlei = Integer.valueOf(params.fenlei)
 
-        def totalSummary = summaryService.getTotalSummary(year, month, day)
+        def totalSummary = summaryService.getTotalSummary(year, month, day, fenlei, fangshi)
 
         render(view:"show", model:[summary:totalSummary])
     }
+
+    def ajaxGetItemSummary = {
+
+        def now = new Date()
+        def year = now.year
+        def month = now.month
+        def day = now.date
+        def fangshi = -1
+        def fenlei = -1
+        def type = 1;
+
+        if (params.year != null) year = Integer.valueOf(params.year)
+        if (params.month != null) month = Integer.valueOf(params.month)
+        if (params.day != null) day = Integer.valueOf(params.day)
+        if (params.fangshi != null) fangshi = Integer.valueOf(params.fangshi)
+        if (params.fenlei != null) fenlei = Integer.valueOf(params.fenlei)
+        if (params.type != null) type = Integer.valueOf(params.type)
+
+        def summary;
+
+        switch(type) {
+            case 1:
+                summary = summaryService.getFenleiYearSummary(year, fenlei, fangshi)
+                break
+            case 2:
+                summary = summaryService.getFenleiMonthSummary(year, month, fenlei, fangshi)
+                break
+            case 3:
+                summary = summaryService.getFenleiTodaySummary(year, month, day, fenlei, fangshi)
+                break
+            default:
+            break
+        }
+
+        render(template:"rangeFenleiSummaryBodyTemplate", model:[summary:summary])
+    }
+
 }
