324ecb1a5ef5e535633d258db4671f2344b4965b
jingnan.si <jingnan.si@fb698924-7d51-0410-b70a-75a81c54cbca>
2010-10-22 12:25:40 +0000

    add entry for python

--- trunk/web/diaryaccount-py/entry/add.py
+++ trunk/web/diaryaccount-py/entry/add.py
@@ -1,13 +1,11 @@
 import json.encoder
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 
 __author__="angelstone"
 __date__ ="$2010-10-10 15:51:09$"
 
 from google.appengine.ext import webapp
 from django.utils import simplejson as json
-
+  
 class BatchAddPage(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
